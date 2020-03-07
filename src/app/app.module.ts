5e94e6babc593b3518232b498722adc7e896acd2
Avishek <choudhury.avishek1@tcs.com>
2020-03-07 21:08:20 +0530

    generated tgz file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DatepickerComponent } from './datepicker/datepicker.component';
-import { ComboboxModule } from '../../projects/combobox/src/lib/combobox.module';
+import { ComboboxModule } from 'combobox';
 @NgModule({
   declarations: [
     AppComponent,

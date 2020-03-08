1dc75d4fbbf6951e150d59bcbf8998f966a1913f
Avishek <choudhury.avishek1@tcs.com>
2020-03-08 23:27:43 +0530

    added sh file

---
+++ copyAssets.sh
@@ -0,0 +1,4 @@
+echo "No yar this is wrong --------"
+echo "Ok No problem copying assets to lib build!"
+cp -r ./projects/combobox/src/assets ./dist/combobox
+echo "Aee Muthu copied the assets"
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-// import { ComboboxModule } from 'angular8simplecombobox';
-import {ComboboxModule} from '../../projects/combobox/src/lib/combobox.module';
+import { ComboboxModule } from 'angular8simplecombobox';
+// import {ComboboxModule} from 'combobox';
+// import {ComboboxModule} from '../../projects/combobox/src/lib/combobox.module';
 @NgModule({
   declarations: [
     AppComponent,

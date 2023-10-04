437c8c1b5657cc94ac7751aa7c20c2b86bbb5102
avishek1291 <avishek1291@gmail.com>
2023-10-04 12:25:00 +0530

    Update copyAssets.sh

--- copyAssets.sh
+++ copyAssets.sh
@@ -1,4 +1,3 @@
-echo "No yar this is wrong --------"
-echo "Ok No problem copying assets to lib build!"
+echo "copying assets to lib build!"
 cp -r ./projects/combobox/src/assets ./dist/combobox
-echo "Aee Muthu copied the assets"
+echo "copied the assets successfully"
